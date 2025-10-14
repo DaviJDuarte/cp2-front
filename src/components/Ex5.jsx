3a57a20ac451890d03195dc4700194a5fcb1966f
Davi Duarte <davijduarte.dev@gmail.com>
2025-10-14 20:40:25 -0300

    Ex5

--- src/components/Ex5.jsx
+++ src/components/Ex5.jsx
@@ -7,9 +7,15 @@
 
 export default function Ex5() {
   return (
-    <div className="my-8">
+    <div className="my-8 mx-4">
         <h1 className="text-2xl font-bold mb-4">Ex5 - Navbar Responsiva</h1>
-      {/* Resolva aqui */}
+        <div className="border border-gray-400 flex justify-center">
+          <ul className="flex flex-col lg:flex-row gap-3">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Sobre</a></li>
+          </ul>
+        </div>
     </div>
   );
 }
