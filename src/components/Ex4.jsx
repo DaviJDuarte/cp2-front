d4a1668f3cc258834955b5f910763b46ffa64a1f
Davi Duarte <davijduarte.dev@gmail.com>
2025-10-14 20:03:41 -0300

    Ex4

--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -4,9 +4,13 @@
 
 export default function Ex4() {
   return (
-    <div className="my-8">
+    <div className="my-8 mx-4">
         <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
-      {/* Resolva aqui */}
+        <div className="flex flex-col gap-2 border w-[100%] lg:w-[24rem] p-2 rounded shadow-lg">
+          <h1 className="font-bold">Título</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos vero, officia ut doloremque consectetur asperiores</p>
+          <button className={'bg-blue-700 hover:brightness-80 cursor-pointer text-white p-2 rounded w-[60px] self-end'}>Botão</button>
+        </div>
     </div>
   );
 }
