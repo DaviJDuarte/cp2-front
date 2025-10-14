fc3b5ded5a8bc3117aa32484ca8b5a269fa59d19
Davi Duarte <davijduarte.dev@gmail.com>
2025-10-14 20:53:58 -0300

    Ex6

--- src/components/Ex6.jsx
+++ src/components/Ex6.jsx
@@ -12,10 +12,10 @@ export default function Ex6() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex6 - Layout com Grid</h1>
-        <div className="h-[20rem] grid grid-rows-3 grid-cols-1">
-          <header className="flex justify-center items-center bg-blue-400 h-[80px]">Header</header>
-          <main className="flex justify-center bg-pink-400 h-auto">Main</main>
-          <footer className="flex justify-center bg-red-400 h-[80px]">Footer</footer>
+        <div className="h-[20rem] grid grid-rows-[80px_1fr_80px]">
+          <header className="flex justify-center items-center bg-blue-400 max-h-[80px]">Header</header>
+          <main className="flex justify-center bg-pink-400 row-span-1">Main</main>
+          <footer className="flex justify-center items-center bg-red-400 max-h-[80px]">Footer</footer>
         </div>
     </div>
   );
